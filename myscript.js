1ea881415ffa1cb7756fd4e6241419991f9d8862
Colin Mason <masonc789@gmail.com>
2020-01-20 23:22:30 -0500

    Upload extension

---
+++ myscript.js
@@ -0,0 +1,17 @@
+
+var google = document.getElementById("navbar");
+    var button = document.createElement("button");
+    button.id = "aButton";
+    var text = document.createTextNode("Show/Hide all answers");
+    button.appendChild(text);
+    google.appendChild(button);
+var theButton = document.getElementById('aButton');
+
+theButton.addEventListener('click', function() {
+				if(document.getElementById("answer_1").style.visibility == "hidden" || document.getElementById("answer_1").style.visibility == ''){
+					for (let el of document.querySelectorAll('.search_correct_response')) el.style.visibility = 'visible';
+				}
+        		else {
+        			for (let el of document.querySelectorAll('.search_correct_response')) el.style.visibility = 'hidden';
+        		}
+            });
