d8c7872681829646520a8db092ea063e73b21919
Colin Mason <masonc789@gmail.com>
2023-11-21 02:29:18 -0500

    ensure button loads after navbar

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Wait for the window to load before executing the script
+window.onload = function() {
   const navbar = document.getElementById('navbar');
   if (!navbar) {
     console.error('Navbar element not found');
@@ -8,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const button = document.createElement('button');
   button.id = 'toggleAnswersBtn';
   button.textContent = 'Show/Hide all answers';
+  
+  // Ensure the button is appended to a visible container
   navbar.appendChild(button);
 
   const answers = document.querySelectorAll('.search_correct_response');
@@ -23,11 +26,10 @@ document.addEventListener('DOMContentLoaded', function() {
       return;
     }
 
-    // Toggle visibility based on the current state of the first answer.
-    // If it's not explicitly set to 'visible', we'll assume it's 'hidden'.
+    // Toggle the visibility
     const isVisible = firstAnswer.style.visibility === 'visible';
     answers.forEach(el => {
       el.style.visibility = isVisible ? 'hidden' : 'visible';
     });
   });
-});
+};
