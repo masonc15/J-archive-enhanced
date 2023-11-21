50f4f45c7553eb4c44c758cfc881fa8143e58bf0
Colin Mason <masonc789@gmail.com>
2023-11-21 02:25:58 -0500

    refactor js for style and clarity

--- myscript.js
+++ myscript.js
@@ -1,33 +1,33 @@
-(function() {
-  const google = document.getElementById("navbar");
-  if (!google) {
-      console.error('Navbar element not found');
-      return;
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.getElementById('navbar');
+  if (!navbar) {
+    console.error('Navbar element not found');
+    return;
   }
-  const button = document.createElement("button");
-  button.id = "aButton";
-  const text = document.createTextNode("Show/Hide all answers");
-  button.appendChild(text);
-  google.appendChild(button);
+
+  const button = document.createElement('button');
+  button.id = 'toggleAnswersBtn';
+  button.textContent = 'Show/Hide all answers';
+  navbar.appendChild(button);
 
   const answers = document.querySelectorAll('.search_correct_response');
   if (answers.length === 0) {
-      console.error('No answer elements found');
-      return;
+    console.error('No answer elements found');
+    return;
   }
 
-  const theButton = document.getElementById('aButton');
-  theButton.addEventListener('click', function () {
-    const firstAnswer = document.getElementById("answer_1");
+  button.addEventListener('click', function() {
+    const firstAnswer = document.getElementById('answer_1');
     if (!firstAnswer) {
       console.error('First answer element not found');
       return;
     }
-  
+
     // Toggle visibility based on the current state of the first answer.
     // If it's not explicitly set to 'visible', we'll assume it's 'hidden'.
-    let visibility = firstAnswer.style.visibility === 'visible' ? 'hidden' : 'visible';
-    answers.forEach(el => el.style.visibility = visibility);
+    const isVisible = firstAnswer.style.visibility === 'visible';
+    answers.forEach(el => {
+      el.style.visibility = isVisible ? 'hidden' : 'visible';
+    });
   });
-  
-})();
+});
