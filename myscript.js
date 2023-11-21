486be7dbaa2d8c608cc7d404e6e25e19c8cd2728
Colin Mason <masonc789@gmail.com>
2023-11-21 02:23:03 -0500

    fix initial button click behavior

--- myscript.js
+++ myscript.js
@@ -1,17 +1,33 @@
-
-var google = document.getElementById("navbar");
-var button = document.createElement("button");
-button.id = "aButton";
-var text = document.createTextNode("Show/Hide all answers");
-button.appendChild(text);
-google.appendChild(button);
-var theButton = document.getElementById('aButton');
-
-theButton.addEventListener('click', function () {
-  if (document.getElementById("answer_1").style.visibility == "hidden" || document.getElementById("answer_1").style.visibility == '') {
-    for (let el of document.querySelectorAll('.search_correct_response')) el.style.visibility = 'visible';
+(function() {
+  const google = document.getElementById("navbar");
+  if (!google) {
+      console.error('Navbar element not found');
+      return;
   }
-  else {
-    for (let el of document.querySelectorAll('.search_correct_response')) el.style.visibility = 'hidden';
+  const button = document.createElement("button");
+  button.id = "aButton";
+  const text = document.createTextNode("Show/Hide all answers");
+  button.appendChild(text);
+  google.appendChild(button);
+
+  const answers = document.querySelectorAll('.search_correct_response');
+  if (answers.length === 0) {
+      console.error('No answer elements found');
+      return;
   }
-});
+
+  const theButton = document.getElementById('aButton');
+  theButton.addEventListener('click', function () {
+    const firstAnswer = document.getElementById("answer_1");
+    if (!firstAnswer) {
+      console.error('First answer element not found');
+      return;
+    }
+  
+    // Toggle visibility based on the current state of the first answer.
+    // If it's not explicitly set to 'visible', we'll assume it's 'hidden'.
+    let visibility = firstAnswer.style.visibility === 'visible' ? 'hidden' : 'visible';
+    answers.forEach(el => el.style.visibility = visibility);
+  });
+  
+})();
