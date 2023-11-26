ea260ecf5ed98fc445ce0c13665a8d1f51feb4c2
Colin Mason <masonc789@gmail.com>
2023-11-26 06:25:21 -0500

    append button to navbar

--- gameAnswerToggle.js
+++ gameAnswerToggle.js
@@ -1,5 +1,6 @@
 // Wait for the window to load before executing the script
 window.onload = function () {
+  const navbar = document.getElementById('navbar');
     if (!navbar) {
         console.error('Navbar element not found');
         return;
@@ -8,5 +9,8 @@ window.onload = function () {
   const button = document.createElement('button');
   button.id = 'toggleAnswersBtn';
   button.textContent = 'Show/Hide all answers';
+
+  // Ensure the button is appended to a visible container
+  navbar.appendChild(button);
     });
 });
