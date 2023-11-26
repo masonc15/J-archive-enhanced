07a75156df897a5bb0ab05be4e26371a51161bee
Colin Mason <masonc789@gmail.com>
2023-11-26 06:57:32 -0500

    add game answers toggle

--- j-archive-enhanced.user.js
+++ j-archive-enhanced.user.js
@@ -34,3 +34,32 @@
             el.style.visibility = isVisible ? 'hidden' : 'visible';
         });
     }
+
+    // Toggle answers on game pages
+    function toggleGameAnswers() {
+        const correctResponses = document.querySelectorAll('.correct_response');
+        correctResponses.forEach(function (correctResponse) {
+            const clueTextTd = correctResponse.closest('tr').querySelector('.clue_text');
+            if (!clueTextTd) return;
+
+            const answerElement = clueTextTd.querySelector('.answer-text');
+            if (answerElement) {
+                // Remove the answer and the breaks
+                clueTextTd.querySelectorAll('.answer-break').forEach(br => clueTextTd.removeChild(br));
+                clueTextTd.removeChild(answerElement);
+            } else {
+                // Append the answer with two line breaks
+                for (let i = 0; i < 2; i++) {
+                    const breakElement = document.createElement('br');
+                    breakElement.className = 'answer-break';
+                    clueTextTd.appendChild(breakElement);
+                }
+                const answerElement = document.createElement('span');
+                answerElement.textContent = correctResponse.textContent;
+                answerElement.className = 'answer-text';
+                answerElement.style.color = '#00FFFF';
+                clueTextTd.appendChild(answerElement);
+            }
+        });
+    }
+
