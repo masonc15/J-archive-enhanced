0e47bfffe3194376d4f0d870327cc30696feb816
Colin Mason <masonc789@gmail.com>
2023-11-26 06:56:32 -0500

    initialize userscript

---
+++ j-archive-enhanced.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         J-Archive Plus Enhanced
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Shows Jeopardy answers on J-Archive more efficiently
+// @author       Colin Mason
+// @match        *://*.j-archive.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
